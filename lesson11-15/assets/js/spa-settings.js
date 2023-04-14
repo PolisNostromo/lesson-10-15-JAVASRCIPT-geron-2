4733f2f724f05ef66286b56458aa844412d228e7
PolisNostromo <122610171+PolisNostromo@users.noreply.github.com>
2023-04-14 21:14:06 +0600

    Add files via upload

---
+++ lesson11-15/assets/js/spa-settings.js
@@ -0,0 +1,50 @@
+const information = [
+    {
+        url: './pages/components/header.html',
+        include: '.wrapper'
+    },
+    {
+        url: './pages/components/main.html',
+        include: '.include-main'
+    },
+    {
+        url: './pages/components/category.html',
+        include: '.include-category'
+    },
+    {
+        url: './pages/components/cards.html',
+        include: '.include-cards'
+    },
+    {
+        url: './pages/components/footer.html',
+        include: '.include-footer'
+    },
+    {
+        url: './pages/product.html',
+        include: '.include-product'
+    },
+    {
+        url: './pages/cart.html',
+        include: '.include-basket'
+    },
+    {
+        url: './pages/order.html',
+        include: '.include-order'
+    }
+]
+
+information.forEach(item=>{
+    fetch(item.url)
+    .then(response =>{
+        return response.text()
+    })
+    .then(data =>{
+        document.querySelector(item.include).innerHTML = data
+        if(item.include === '.include-cards'){
+            setCards()
+        }
+        if(item.include === '.include-basket'){
+            cardsBasket()
+        }
+    })
+})
